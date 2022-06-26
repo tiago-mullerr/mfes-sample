84447c421a29b06a239dbe465cb32bea57077483
Tiago Muller <tiago@Tiagos-MacBook-Pro.local>
2022-06-26 12:33:02 -0300

    Removing blog mfe

--- blog/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import '../components/BlogCard/BlogCard.modules.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- main/src/components/Navbar/Navbar.js
+++ main/src/components/Navbar/Navbar.js
@@ -4,14 +4,12 @@ import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        // <div className={classes["nav-bar"]}>
-            <div className={classes["nav-bar-links"]}>
-                <Link to='/'><span>Home</span></Link>
-                <Link to='/about'><span>About</span></Link>
-                <Link to='/description'><span>Description</span></Link>
-                <Link to='/users'><span>Users</span></Link>
-            </div>
-        // </div>
+        <div className={classes["nav-bar-links"]}>
+            <Link to='/'><span>Home</span></Link>
+            <Link to='/about'><span>About</span></Link>
+            <Link to='/description'><span>Description</span></Link>
+            <Link to='/users'><span>Users</span></Link>
+        </div>
     );
 }
 
